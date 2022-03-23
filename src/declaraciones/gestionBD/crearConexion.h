b374ec08ae7a150996cc26bf40c815b45bbec43b
GiovanniLocatelli <giovannilocatelli@opendeusto.es>
2022-03-23 14:26:27 +0100

    Conexión base de datos

---
+++ src/declaraciones/gestionBD/crearConexion.h
@@ -0,0 +1,9 @@
+#include "Sqlite/sqlite3.h"
+#ifndef dbConnection
+#define dbConnection
+
+extern sqlite3 *db;
+
+int startConn();
+
+#endif
---
+++ src/implementaciones/gestionBD/crearConexion.c
@@ -0,0 +1,21 @@
+#include "sqlite/sqlite3.h"
+#include "crearConexion.h"
+#include <stdio.h>
+
+sqlite3 *db;
+
+int startConn()
+{
+    int rc = sqlite3_open("./db/base.db", &db);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n",
+                sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 0;
+    }
+
+    return 1;
+}
