2d356485793524fe787c32d31c360d4eedb47d45
Alex López <alex.lopezdelacalle@opendeusto.es>
2022-03-28 11:25:27 +0200

    getData

---
+++ src/implementaciones/gestionBD/getData.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../declaraciones/gestionBD/getData.h"
+#include "../../declaraciones/gestionBD/crearConexion.h"
+#include "../../../librerias/Sqlite/sqlite3.h"
+#include "../../declaraciones/logicaDeDatos/estructuras.h"
+
+int numFilas = 1;
+
+Usuario **getListaUsuario(){
+	
+}
